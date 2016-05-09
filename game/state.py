75c003f23ffcdbafbaf13955d62cc0bcb18fb30e
Michael Sender <msender98@gmail.com>
2016-05-08 21:52:25 -0400

    We're really doing it guys. This is so fun.

---
+++ game/state.py
@@ -0,0 +1,19 @@
+class State(object):
+
+    def __init__(self):
+        self.organisms = []
+        self.size = {'X': 1000,'Y': 1000}
+
+    def transition(self, actions):
+        pass
+
+        
+
+class Organism(object):
+    pass
+
+class Action(object):
+    pass
+
+
+
