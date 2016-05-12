6a099fa1a06811d7873438d8a1f356dfdbd369bf
Michael Sender <msender98@gmail.com>
2016-05-11 21:02:48 -0400

    Updated State.py

--- game/state.py
+++ game/state.py
@@ -4,22 +4,35 @@ class State(object):
         self.organisms = []
         self.width = width
         self.height = height
+        self.food = []
 
 
     def transition(self, actions):
         pass
 
-        
+    def __str__(self): #Print the game board ---/n---/n
+        pass
 
 class Organism(object):
     
-    def __init__(self,cells):
+    def __init__(self, cells, foodstoreinit):
         self.cells = cells
+        self.foodstore = foodstoreinit
 
     def add_cells(self, cells):
         self.cells += cells
 
     
+
+class Food(object):
+    def __init__(self, cells):
+        self.cells = cells
+
+    def add_cells(self, cells):
+        self.cells += cells
+
+    def delete_cells(self, cells):
+        self.cells -=
 
 class Action(object):
     pass
