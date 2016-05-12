a57c8d0e1479aba2c285a40b0f81bdb666fe1f6d
Michael Sender <msender98@gmail.com>
2016-05-11 20:34:59 -0400

    Added a default state thing. For you know...

--- game/default.py
+++ game/default.py
@@ -1,5 +1,20 @@
+import state
+
 # get_default_state returns an instance of game.State that's 100 x 100
 # and contains a single organism with 9 cells on the square with opposite
 # corners at 0, 0 and 2, 2.
 def get_default_state():
-    pass
+	sta = state.State(100, 100)
+	org = state.Organism([])
+	for x in range(3):
+		for y in range(3):
+			org.add_cells([(x, y)])
+	return org
+
+org = get_default_state()
+
+print org.cells
+
+
+
+    
