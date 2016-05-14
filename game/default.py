c27f09aeaefe6d8fcb506fa322e10b05b6c9593f
Michael Sender <msender98@gmail.com>
2016-05-13 23:28:22 -0400

    Default game to test the transition function and a simple transition just with eating food

--- game/default.py
+++ game/default.py
@@ -1,19 +1,41 @@
 import state
-
+import time
 # get_default_state returns an instance of game.State that's 100 x 100
 # and contains a single organism with 9 cells on the square with opposite
 # corners at 0, 0 and 2, 2.
 def get_default_state():
-	sta = state.State(100, 100)
-	org = state.Organism([])
+	org1 = state.Organism([],100)
+	
 	for x in range(3):
 		for y in range(3):
-			org.add_cells([(x, y)])
-	return org
+			org1.add_cells([(x, y)])
+			
+	food1 = state.Food([(1,2)])
+	food1.add_cells([(2,1)])
 
-org = get_default_state()
+	default_state = state.State(100, 100)
+	default_state.organisms = [org1]
+	default_state.foods = [food1]
+	return default_state
+	
 
-print org.cells
+def run_game():
+	default_state = get_default_state()
+
+	for turns in range(15):
+		default_state.transition([])
+		if len(default_state.organisms) >0:
+			print default_state.organisms[0].cells
+			print default_state.foods[0].cells
+		else:
+			print 'Game Over'
+
+	
+run_game()
+
+
+
+
 
 
 
--- game/state.py
+++ game/state.py
@@ -4,14 +4,31 @@ class State(object):
         self.organisms = []
         self.width = width
         self.height = height
-        self.food = []
+        self.foods = []
 
 
     def transition(self, actions):
+        for org in self.organisms:
+            for foo in self.foods:
+                eat_food = list(set(org.cells) & set(foo.cells))
+                
+                if len(eat_food)>0:
+                    org.foodstore += 1
+                    foo.delete_cells(eat_food[0])
+            org.foodstore -= 1 
+            if org.foodstore < 90:
+                org.delete_cells(org.cells[0])
+            if len(org.cells) == 0:
+                self.organisms.remove(org)
+
+
+
+
+       
+
+    def __str__(self): 
         pass
 
-    def __str__(self): #Print the game board ---/n---/n
-        pass
 
 class Organism(object):
     
@@ -22,9 +39,11 @@ class Organism(object):
     def add_cells(self, cells):
         self.cells += cells
 
-    
+    def delete_cells(self, cells):
+        self.cells.remove(cells)
 
 class Food(object):
+
     def __init__(self, cells):
         self.cells = cells
 
@@ -32,7 +51,7 @@ class Food(object):
         self.cells += cells
 
     def delete_cells(self, cells):
-        self.cells -=
+        self.cells.remove(cells)
 
 class Action(object):
     pass
